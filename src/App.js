02fe6f59ef972e6b33be38f940fa14771e721ca7
Netra Chamlagai <nayan@Netras-MacBook-Pro.local>
2021-01-26 17:57:30 -0500

    file add

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import Game from './components/game/game'
+
+function App(props) {
+    const raccoons = {
+      name: 'Russiaville Raccoons',
+      logoSrc: './assets/images/raccoon.png'
+
+    }
+  
+    const squirrels = {
+      name: 'Sheridan Squirrels',
+      logoSrc: './assets/images/squirrel.png'
+    }
+  
+    const bunnies = {
+      name: 'Burlington Bunnies',
+      logoSrc: './assets/images/bunny.png'
+    }
+  
+    const hounds = {
+      name: 'Hammond Hounds',
+      logoSrc: './assets/images/hound.png'
+    }
+  
+    return (
+      <div className="App">
+        <Game
+          venue="Union 525 Gem"
+          homeTeam={squirrels}
+          visitingTeam={raccoons}
+        />
+        <Game
+          venue="Sheridan Arena"
+          homeTeam={bunnies}
+          visitingTeam={hounds}
+        />
+      </div>
+    )
+  }
+  export default App; 
